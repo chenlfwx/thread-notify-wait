7c6d283d8cce5eb82637e59242b26f280e5f0d95
chendian <chenlflinfool@foxmail.com>
2019-06-15 01:12:43 +0800

    Initial commit

---
+++ src/main/java/com/chen/nosafe/ProductConsumerNoSafe.java
@@ -0,0 +1,59 @@
+package com.chen.nosafe;
+
+/**	线程之间通信，不安全版本
+ *
+ */
+
+public class ProductConsumerNoSafe{
+	public static void main(String[] args){
+		Resource resource = new Resource();
+		Input input = new Input(resource);
+		Output output = new Output(resource);
+		Thread product = new Thread(input);
+		Thread consumer = new Thread(output);
+		product.start();
+		consumer.start();
+	}
+}
+class Resource{
+	String name; // 没有私有化，简便访问。
+	char sex;
+}
+
+class Input implements Runnable{
+	private Resource resource;
+	
+	public Input(Resource resource){
+		this.resource = resource;
+	}
+	@Override
+	public void run(){
+		boolean flag = true;
+		while(true){
+			if(flag){
+				resource.name = "张三";
+				resource.sex = '男';
+				flag = false;
+			}else{
+				resource.name = "李四";
+				resource.sex = '女';
+				flag = true;
+			}
+		}
+	}
+}
+
+class Output implements Runnable{
+	
+	private Resource resource;
+	
+	public Output(Resource resource){
+		this.resource = resource;
+	}
+	@Override
+	public void run(){
+		while(true){
+			System.out.println(resource.name + "---" + resource.sex);
+		}
+	}
+}
---
+++ src/main/java/com/chen/safe/ProductConsumerSafe.java
@@ -0,0 +1,64 @@
+package com.chen.safe;
+
+/**	线程之间通信，安全版本
+ *
+ */
+
+public class ProductConsumerSafe{
+	public static void main(String[] args){
+		Resource resource = new Resource();
+		Input input = new Input(resource);
+		Output output = new Output(resource);
+		Thread product = new Thread(input);
+		Thread consumer = new Thread(output);
+		product.start();
+		consumer.start();
+	}
+}
+
+class Resource{
+	String name; // 没有私有化，简便访问。
+	char sex;
+}
+
+class Input implements Runnable{
+	private Resource resource;
+	
+	public Input(Resource resource){
+		this.resource = resource;
+	}
+	@Override
+	public void run(){
+		boolean flag = true;
+		while(true){
+			synchronized (resource){
+				if(flag){
+					resource.name = "张三";
+					resource.sex = '男';
+					flag = false;
+				}else{
+					resource.name = "李四";
+					resource.sex = '女';
+					flag = true;
+				}
+			}
+		}
+	}
+}
+
+class Output implements Runnable{
+	
+	private Resource resource;
+	
+	public Output(Resource resource){
+		this.resource = resource;
+	}
+	@Override
+	public void run(){
+		while(true){
+			synchronized (resource){
+				System.out.println(resource.name + "---" + resource.sex);
+			}
+		}
+	}
+}
